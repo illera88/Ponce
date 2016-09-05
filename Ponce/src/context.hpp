09db182eac148862493ee58a748d404b620da8bf
mrnobody <agarciaillera@gmail.com>
2016-09-05 03:39:01 -0700

    fixed #21

--- Ponce/src/context.hpp
+++ Ponce/src/context.hpp
@@ -2,7 +2,7 @@
 //Triton
 #include <api.hpp>
 
-void __stdcall needConcreteMemoryValue(triton::arch::MemoryAccess& mem);
-void __stdcall needConcreteRegisterValue(triton::arch::Register& reg);
+void needConcreteMemoryValue(triton::arch::MemoryAccess& mem);
+void needConcreteRegisterValue(triton::arch::Register& reg);
 triton::uint128 getCurrentMemoryValue(triton::__uint addr, triton::uint32 size);
 triton::uint512 getCurrentRegisterValue(triton::arch::Register& reg);
