11ee622b86f78148c57822988d80474c2fdcf491
Francisco Oca <xyzthor@gmail.com>
2016-11-22 13:20:31 -0800

    Update version_number_update.sh

--- version_number_update.sh
+++ version_number_update.sh
@@ -1,5 +1,5 @@
 #script to update the version with the last commit number
-MAIN_VERSION_NUMBER="0.1"
+MAIN_VERSION_NUMBER="0.2"
 DATE=`date +"%Y%m%d"`
 COMMIT=`git rev-parse HEAD|cut -c 1-7`
 FINAL_VERSION="$MAIN_VERSION_NUMBER.$DATE.$COMMIT"
