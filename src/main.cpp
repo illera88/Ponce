99093f8fc9af0d76c899216e26a23155d837792f
Alberto Garcia Illera <agarciaillera@gmail.com>
2022-10-24 22:02:50 +1100

    CI build for all versions

--- src/main.cpp
+++ src/main.cpp
@@ -131,7 +131,11 @@ int idaapi init(void)
     char version[8];
     //We do some checks with the versions...
     if (get_kernel_version(version, sizeof(version))) {
-#if IDA_SDK_VERSION == 770
+#if IDA_SDK_VERSION == 810
+      if (strcmp(version, "8.1") != 0) {
+#elif IDA_SDK_VERSION == 800
+      if (strcmp(version, "8.0") != 0) {
+#elif IDA_SDK_VERSION == 770
         if (strcmp(version, "7.7") != 0) {
 #elif IDA_SDK_VERSION == 760
         if (strcmp(version, "7.6") != 0) {
